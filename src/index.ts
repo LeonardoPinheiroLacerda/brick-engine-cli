e962bdb9fe9af59ac68e1c49b05c8fae2a6f136f
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-02-25 21:05:07 -0300

    chore: Hardcode CLI version to 1.0.8, removing the dependency on `package.json` for version retrieval.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,10 @@ import chalk from "chalk";
 import { readFileSync } from "fs";
 
 export function run() {
-  const pkg = JSON.parse(readFileSync("package.json", "utf-8"));
-
   program
     .name("brick-engine-cli")
     .description("CLI to scaffold Brick Engine projects")
-    .version(pkg.version);
+    .version("1.0.8");
 
   program
     .command("init <name>")
